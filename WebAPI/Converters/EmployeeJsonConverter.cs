04da1aa65f4a0628dda3685fa89fccfca4fb2e31
Vadim Boganov <vadim_boganov@epam.com>
2021-02-02 12:09:41 +0300

    Add converters

---
+++ WebAPI/Converters/EmployeeJsonConverter.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using WebAPI.Models;
+
+namespace WebAPI.Converters
+{
+    public class EmployeeJsonConverter : JsonCreationConverter<Employee>
+    {
+        protected override Employee Create(Type objectType, JObject jObject)
+        {
+            if (jObject == null)
+                throw new ArgumentNullException("Jobject can not be null...");
+
+            if (jObject["discriminator"].ToString() == nameof(HourlyEmployee))
+                return new HourlyEmployee();
+
+            else if (jObject["discriminator"].ToString() == nameof(FixedEmployee))
+                return new FixedEmployee();
+
+            else
+                throw new Exception("Implementation doesn't exist...");
+
+        }
+    }
+}
---
+++ WebAPI/Converters/JsonCreationConverter.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+
+namespace WebAPI.Converters
+{
+    public abstract class JsonCreationConverter<T> : JsonConverter
+    {
+        public override bool CanWrite => false;
+
+        public override bool CanConvert(Type objectType)
+        {
+            return typeof(T).IsAssignableFrom(objectType);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            if (reader == null)
+                throw new ArgumentNullException("Json reader can not be null...");
+
+            if (serializer == null)
+                throw new ArgumentNullException("Serializer can not be null...");
+
+            if (reader.TokenType == JsonToken.Null)
+                return null;
+
+            var jObject = JObject.Load(reader);
+            var target = Create(objectType, jObject);
+            serializer.Populate(jObject.CreateReader(), target);
+            return target;
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected abstract T Create(Type objectType, JObject jObject);
+    }
+}
